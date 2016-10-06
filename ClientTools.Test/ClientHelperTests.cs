043eee9c0e7c6b157e5458a25928f15a99d2d714
Hein <duplessish@gmail.com>
2016-10-06 22:46:48 +0200

    Added some tests

---
+++ ClientTools.Test/ClientHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTools.Test
+{
+    [TestClass]
+    public class ClientHelperTests
+    {
+        [TestMethod]
+        public void TestIsClientHeaderRow()
+        {
+            var testString = "FirstName,LastName,Address,PhoneNumber";
+
+            var client = new Client(testString);
+            var result = client.IsClientHeaderRow();
+
+            Assert.AreEqual(result, true);
+        }
+
+
+        [TestMethod]
+        public void TestIsClientHeaderRow2()
+        {
+            var testString = "FirstName1,LastName,Address,PhoneNumber";
+
+            var client = new Client(testString);
+            var result = client.IsClientHeaderRow();
+
+            Assert.AreEqual(result, false);
+        }
+    }
+}
